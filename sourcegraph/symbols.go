85cacc45f87222a981031d00a96cd7f5a65a4650
Quinn Slack <qslack@qslack.com>
2014-07-30 01:11:43 -0700

    add SrcHTML doc

--- sourcegraph/symbols.go
+++ sourcegraph/symbols.go
@@ -233,6 +233,9 @@ func (s *symbolsService) List(opt *SymbolListOptions) ([]*Symbol, Response, erro
 // Example is a usage example of a symbol.
 type Example struct {
 	graph.Ref
+
+	// SrcHTML is the formatted HTML source code of the example, with links to
+	// definitions.
 	SrcHTML template.HTML
 
 	// The line that the given example starts on
