6a761515aee7185b3a49fe1a5668cf2a9a112046
Quinn Slack <qslack@qslack.com>
2014-12-12 08:56:23 -0800

    Person ShortName

--- sourcegraph/people.go
+++ sourcegraph/people.go
@@ -24,6 +24,19 @@ type Person struct {
 	AvatarURL string
 }
 
+// ShortName returns the person's Login if nonempty and otherwise
+// returns the portion of Email before the '@'.
+func (p *Person) ShortName() string {
+	if p.Login != "" {
+		return p.Login
+	}
+	at := strings.Index(p.Email, "@")
+	if at == -1 {
+		return "(anonymous)"
+	}
+	return p.Email[:at]
+}
+
 // Transient is true if this person was constructed on the fly and is
 // not persisted or resolved to a Sourcegraph/GitHub/etc. user.
 func (p *Person) Transient() bool { return p.UID == 0 }
