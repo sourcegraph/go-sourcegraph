8c3ac15a33892c28aee98210f775240e651f2872
Quinn Slack <qslack@qslack.com>
2015-01-14 21:46:30 -0800

    retain token types during JSON serialization

--- sourcegraph/query_etc.go
+++ sourcegraph/query_etc.go
@@ -33,11 +33,29 @@ func (e ResolveErrors) Fatal() bool { return true }
 // A ResolveError is returned inside a ResolveErrors list by
 // Resolve and occurs when query validation fails.
 type ResolveError struct {
-	Token         // the token that caused the error
+	Token  Token  // the token that caused the error
 	Reason string // the public, user-readable error message to display
 }
 
 func (e ResolveError) Error() string { return fmt.Sprintf("%s (%v)", e.Reason, e.Token) }
+
+type jsonResolveError struct {
+	Token  jsonToken
+	Reason string
+}
+
+func (e ResolveError) MarshalJSON() ([]byte, error) {
+	return json.Marshal(jsonResolveError{jsonToken{e.Token}, e.Reason})
+}
+
+func (e *ResolveError) UnmarshalJSON(b []byte) error {
+	var jv jsonResolveError
+	if err := json.Unmarshal(b, &jv); err != nil {
+		return err
+	}
+	*e = ResolveError{jv.Token.Token, jv.Reason}
+	return nil
+}
 
 // IsResolveError returns true if err is a ResolveErrors list or a
 // single ResolveError.
