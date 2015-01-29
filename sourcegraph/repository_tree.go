f136ce5f228cb50787e60ed8842d1ef300397069
gbbr <ga@stripetree.co.uk>
2015-01-30 01:51:25 +0200

    Initial source code structure

--- sourcegraph/repository_tree.go
+++ sourcegraph/repository_tree.go
@@ -42,10 +42,53 @@ type TreeEntry struct {
 
 	*vcsclient.FileRange // only set for files
 
-	ContentsString string
+	ContentsString string `json:",omitempty"`
+
+	// SourceCode is set when SourceAsJSON is enabled in RepoTreeGetOptions.
+	SourceCode *SourceCode `json:",omitempty"`
 
 	// FormatResult is only set if this TreeEntry is a file.
 	FormatResult *FormatResult `json:",omitempty"`
+}
+
+// SourceCode contains a snippet of code with linked and classed tokens,
+// along with other information about the contents.
+type SourceCode struct {
+	// Lines contains all the lines of the contained code snippet.
+	Lines []*SourceCodeLine `json:"lines,omitempty"`
+
+	// Error contains any error that might have occured during the operation.
+	Error error `json:"error,omitempty"`
+}
+
+// SourceCodeLine contains all tokens on this line along with other information
+// such as byte offsets in original source.
+type SourceCodeLine struct {
+	// StartByte and EndByte are the start and end offsets in bytes, in the original file.
+	StartByte int `json:"s"`
+	EndByte   int `json:"e"`
+
+	// Tokens contains any tokens that may be on this line, including whitespace. Whitespace
+	// is stored as an HTML encoded "string" and token information is stored as
+	// "SourceCodeToken". New lines ('\n') are not present.
+	Tokens []interface{} `json:"t"`
+}
+
+// SourceCodeToken contains information about a code token.
+type SourceCodeToken struct {
+	// URL specifies that the token is a reference or a definition,  based on the
+	// IsDef property.
+	URL string `json:"u,omitempty"`
+
+	// IsDef specifies whether the token is a definition.
+	IsDef bool `json:"d,omitempty"`
+
+	// Class specifies the token type as per
+	// [google-code-prettify](https://code.google.com/p/google-code-prettify/).
+	Class string `json:"s"`
+
+	// Label is non-whitespace HTML encoded source code.
+	Label string `json:"h"`
 }
 
 // FormatResult contains information about and warnings from the formatting
@@ -68,8 +111,12 @@ type FormatResult struct {
 // RepoTreeGetOptions specifies options for (RepoTreeService).Get.
 type RepoTreeGetOptions struct {
 	// Formatted is whether the specified entry, if it's a file, should have its
-	// contents code-formatted.
+	// Contents code-formatted with HTML. This option has no effect if SourceAsJSON is set.
 	Formatted bool
+
+	// SourceAsJSON returns the source code tokenized and linked, in JSON format,
+	// as an array of lines that contain information about their contents and tokens.
+	SourceAsJSON bool `url:",omitempty"`
 
 	ContentsAsString bool `url:",omitempty"`
 
