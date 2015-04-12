35634dd89c66d1db5d39dd5af2818d98ae51a6e5
Quinn Slack <qslack@qslack.com>
2015-04-12 11:32:30 -0700

    move DefListOptions Offset test from sourcegraph repo to go-sourcegraph repo

--- sourcegraph/defs_test.go
+++ sourcegraph/defs_test.go
@@ -7,6 +7,7 @@ import (
 
 	"sourcegraph.com/sourcegraph/go-sourcegraph/router"
 	"sourcegraph.com/sourcegraph/srclib/graph"
+	"sourcegraph.com/sourcegraph/srclib/store"
 )
 
 func TestDefsService_Get(t *testing.T) {
@@ -83,6 +84,60 @@ func TestDefsService_List(t *testing.T) {
 
 	if !reflect.DeepEqual(defs, want) {
 		t.Errorf("Defs.List returned %+v, want %+v", defs, want)
+	}
+}
+
+func TestDefListOptions_Offset(t *testing.T) {
+	adjDefs := []*graph.Def{
+		{
+			DefKey: graph.DefKey{
+				Path: "p/1",
+			},
+			Name:     "d1",
+			DefStart: 10,
+			DefEnd:   20,
+			Data:     []byte(`{}`),
+		},
+		{
+			DefKey: graph.DefKey{
+				Path: "p/2",
+			},
+			Name:     "d1",
+			DefStart: 10,
+			DefEnd:   20,
+			Data:     []byte(`{}`),
+		},
+		{
+			DefKey: graph.DefKey{
+				Path: "q",
+			},
+			Name:     "d2",
+			DefStart: 10,
+			DefEnd:   20,
+			Data:     []byte(`{}`),
+		},
+		{
+			DefKey: graph.DefKey{
+				Path: "p",
+			},
+			Name:     "d1",
+			DefStart: 40,
+			DefEnd:   50,
+			Data:     []byte(`{}`),
+		},
+	}
+
+	opt := &DefListOptions{
+		Name:      adjDefs[0].Name,
+		ByteStart: adjDefs[0].DefStart,
+		ByteEnd:   adjDefs[0].DefEnd,
+	}
+
+	defs := store.DefFilters(opt.DefFilters()).SelectDefs(adjDefs...)
+
+	wantDefs := []*graph.Def{adjDefs[0], adjDefs[1]}
+	if !reflect.DeepEqual(defs, wantDefs) {
+		t.Errorf("got %+v, want %+v", defs, wantDefs)
 	}
 }
 
