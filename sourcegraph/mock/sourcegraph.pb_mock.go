81abf76f0e634ef016d99d576aeac9f7c6056fa0
Quinn Slack <qslack@qslack.com>
2015-04-24 23:21:58 -0700

    remove RefreshVCS method from Repos (is on MirrorRepos)

--- sourcegraph/mock/sourcegraph.pb_mock.go
+++ sourcegraph/mock/sourcegraph.pb_mock.go
@@ -60,7 +60,6 @@ type ReposServer struct {
 	ListCommits_  func(v0 context.Context, v1 *sourcegraph.ReposListCommitsOp) (*sourcegraph.CommitList, error)
 	ListBranches_ func(v0 context.Context, v1 *sourcegraph.ReposListBranchesOp) (*sourcegraph.BranchList, error)
 	ListTags_     func(v0 context.Context, v1 *sourcegraph.ReposListTagsOp) (*sourcegraph.TagList, error)
-	RefreshVCS_   func(v0 context.Context, v1 *sourcegraph.RepoSpec) (*pbtypes.Void, error)
 }
 
 func (s *ReposServer) Get(v0 context.Context, v1 *sourcegraph.RepoSpec) (*sourcegraph.Repo, error) {
@@ -97,10 +96,6 @@ func (s *ReposServer) ListBranches(v0 context.Context, v1 *sourcegraph.ReposList
 
 func (s *ReposServer) ListTags(v0 context.Context, v1 *sourcegraph.ReposListTagsOp) (*sourcegraph.TagList, error) {
 	return s.ListTags_(v0, v1)
-}
-
-func (s *ReposServer) RefreshVCS(v0 context.Context, v1 *sourcegraph.RepoSpec) (*pbtypes.Void, error) {
-	return s.RefreshVCS_(v0, v1)
 }
 
 var _ sourcegraph.ReposServer = (*ReposServer)(nil)
