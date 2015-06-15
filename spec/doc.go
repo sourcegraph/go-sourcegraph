5684da901ecaf04a0175ff83ac5c77fba4b6eeed
Quinn Slack <qslack@qslack.com>
2015-06-15 05:48:09 -0700

    package docs

---
+++ spec/doc.go
@@ -0,0 +1,5 @@
+// Package spec contains regexps, parse functions, and stringification
+// functions for user, repo, repo rev, etc., specifiers.
+//
+// It is independent of URL routing and protobufs for simplicity.
+package spec
