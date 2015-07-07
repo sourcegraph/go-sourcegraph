01bf70983d1af724913ce5768e7db0656c6e0f59
Quinn Slack <qslack@qslack.com>
2015-07-07 00:35:27 -0700

    use per-target conn pool in grpc dialer (avoid connect races)

--- sourcegraph/conn_pool.go
+++ sourcegraph/conn_pool.go
@@ -7,12 +7,36 @@ import (
 )
 
 var (
+	connTargetMusMu sync.Mutex
+	connTargetMus   map[string]*sync.Mutex
+
 	connsMu sync.Mutex
 	conns   map[string]*grpc.ClientConn // keyed on GRPC target (i.e., addr)
 )
 
+// lockTargetMutex creates the mutex for target (if it doesn't already
+// exist) and obtains the lock. It is used to implement per-target
+// locks for pooledGRPCDial.
+func lockTargetMutex(target string) *sync.Mutex {
+	connTargetMusMu.Lock()
+	if connTargetMus == nil {
+		connTargetMus = map[string]*sync.Mutex{}
+	}
+	if _, ok := connTargetMus[target]; !ok {
+		connTargetMus[target] = new(sync.Mutex)
+	}
+	mu := connTargetMus[target]
+	mu.Lock()
+	connTargetMusMu.Unlock()
+	return mu
+}
+
 // pooledGRPCDial is a global connection pool for grpc.Dial.
 func pooledGRPCDial(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	// Make sure we are the only goroutine dealing with this target.
+	targetMu := lockTargetMutex(target)
+	defer targetMu.Unlock()
+
 	connsMu.Lock()
 	if conns == nil {
 		conns = map[string]*grpc.ClientConn{}
@@ -29,13 +53,6 @@ func pooledGRPCDial(target string, opts ...grpc.DialOption) (*grpc.ClientConn, e
 	}
 
 	connsMu.Lock()
-	if existing := conns[target]; existing != nil {
-		// Another goroutine beat us to establishing the connection;
-		// close ours and return the winner's.
-		connsMu.Unlock()
-		conn.Close()
-		return existing, nil
-	}
 	conns[target] = conn
 	connsMu.Unlock()
 
